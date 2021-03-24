032d9c7df0001d0f686d4201fd55d86e4a554540
tanveer_h <tanveer.h@bitmascot.com>
2021-03-24 12:01:49 +0600

    Add project files.

---
+++ Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SnackMachine.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
