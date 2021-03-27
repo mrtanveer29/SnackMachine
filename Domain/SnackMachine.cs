31f3d4d7b48f0ec09db3ec72bb77c23175a284e7
Tanveer Hasan <mrtanveer.29@gmail.com>
2021-03-27 18:16:02 +0600

    a money abstruction is added

--- Domain/SnackMachine.cs
+++ Domain/SnackMachine.cs
@@ -1,63 +1,28 @@
-﻿using System;
+﻿using Domain.Logic;
+using System;
 
 namespace Domain
 {
     public sealed class SnackMachine
     {
-        public int OneCentCount { get; private set; }
-        public int TenCentCount { get; private set; }
-        public int QuarterCount { get; private set; }
-        public int OneDollarCount { get; private set; }
-        public int FiveDollarCount { get; private set; }
-        public int TwentyDollarCount { get; private set; }
 
-        public int OneCentInTransaction { get; private set; }
-        public int TenCentInTransaction { get; private set; }
-        public int QuarterInTransaction { get; private set; }
-        public int OneDollarInTransaction { get; private set; }
-        public int FiveDollarInTransaction { get; private set; }
-        public int TwentyDollarInTransaction { get; private set; }
+        public Money MoneyInside { get; private set; }
+        public Money MoneyInTransaction { get; private set; }
 
-        public void InsertMoney(int oneCentCount,
-                                int tenCentCount,
-                                int quarterCount,
-                                int oneDollarCount,
-                                int tenDollarCount,
-                                int twentyDollarCount)
+        public void InsertMoney(Money moneyInserted)
         {
-            OneCentInTransaction += oneCentCount;
-            TenCentInTransaction += tenCentCount;
-            QuarterInTransaction += quarterCount;
-            OneDollarInTransaction += oneDollarCount;
-            FiveDollarInTransaction += FiveDollarCount;
-            TwentyDollarInTransaction += twentyDollarCount;
+            MoneyInTransaction += moneyInserted;
         }
 
         public void ReturnMoney()
         {
-            OneCentInTransaction = 0;
-            TenCentInTransaction = 0;
-            QuarterInTransaction = 0;
-            OneDollarInTransaction = 0;
-            FiveDollarInTransaction = 0;
-            TwentyDollarInTransaction = 0;
+           //MoneyInTransaction=0;
         }
 
         public void BuyFood()
         {
-            OneCentCount += OneCentInTransaction;
-            TenCentCount += TenCentInTransaction;
-            QuarterCount += QuarterInTransaction;
-            OneDollarCount += OneDollarInTransaction;
-            FiveDollarCount += FiveDollarInTransaction;
-            TwentyDollarCount += TwentyDollarInTransaction;
-
-            OneCentInTransaction = 0;
-            TenCentInTransaction = 0;
-            QuarterInTransaction = 0;
-            OneDollarInTransaction = 0;
-            FiveDollarInTransaction = 0;
-            TwentyDollarInTransaction = 0;
+            MoneyInside += MoneyInTransaction;
+            //MoneyInTransaction = 0;
         }
 
     }
